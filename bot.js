e57717869475eff73955aea61e996d8f4d5fe1a3
Jialong Chen <jlch@brandeis.edu>
2020-08-22 14:15:04 -0700

    minor change

--- bot.js
+++ bot.js
@@ -1267,7 +1267,7 @@ function getstatus(table, memberName) {
 
 function getOrgStatus(table, memberName) {
     let row = 0;
-    console.log('table ', table, ' member ', member)
+    console.log('table ', table, ' member ', memberName)
     for (; row < table.length; row++) {
         if (table[row][0] == memberName) break
     }
