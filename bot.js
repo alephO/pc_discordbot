28b159df458bafa3719bd0e5a583d16bf4c5957f
Jialong Chen <jlch@brandeis.edu>
2020-08-23 02:26:33 -0700

    Add update members

--- bot.js
+++ bot.js
@@ -96,6 +96,11 @@ client.on('ready', async () => {
     }
     console.log(userlist);
     console.log(client.user.username + " is ready.");
+    for(let guild of client.guilds){
+        for(let mem of guild.members){
+            console.log(mem)
+        }
+    }
 });
 
 
